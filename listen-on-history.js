e286232af2a28ff5e04a784b4a771916eb546697
Alexander Lechner <alexander.lechner.extern@atruvia.de>
2024-02-05 10:05:31 +0100

    filter navigation information

--- listen-on-history.js
+++ listen-on-history.js
@@ -1,25 +1,29 @@
-const filter = {
-    url: [{hostContains: "example.com"}, {hostPrefix: "developer"}],
-};
-
-function logOnHistoryStateUpdated (details) {
-        const table = document.getElementById("history");
-        const row = document.createElement("tr");
-        const cell = document.createElement("td");
-        row.insertCell().textContent = JSON.stringify(details);
-        table.appendChild(row);
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
-        console.log('request', request);
-        console.log('sender', sender);
-        logOnHistoryStateUpdated(request)
-        sendResponse();
-});
-
-
-chrome.webNavigation.onHistoryStateUpdated.addListener(
-    logOnHistoryStateUpdated,
-    /*filter,*/
-);
-
+function logOnHistoryStateUpdated(transform) {
+    return function (details) {
+      const table = document.getElementById("history");
+      const row = document.createElement("tr");
+      const cell = document.createElement("td");
+      const data = transform ? transform(details) : details;
+      row.insertCell().textContent = JSON.stringify(
+        data
+      );
+      table.appendChild(row);
+    };
+  }
+  
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("request", request);
+    console.log("sender", sender);
+    logOnHistoryStateUpdated()(request);
+    sendResponse();
+  });
+  
+  chrome.webNavigation.onHistoryStateUpdated.addListener(
+    logOnHistoryStateUpdated((details) => ({
+      transitionQualifiers: details.transitionQualifiers,
+      transitionType:details.transitionType,
+      url: details.url,
+    }))
+  
+  );
+  
